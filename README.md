39a19577d02e0b4eebdd74ec81b258569d06fbfc
Robert O'Callahan <robert@ocallahan.org>
2020-12-22 15:50:45 +1300

    Commit working tool

---
+++ README.md
@@ -0,0 +1,55 @@
+# on-prem
+
+Tools for running Pernosco on-premises.
+
+## Quick start guide
+
+You will need AWS credentials to download Pernosco container images, i.e. values for environment variables `AWS_ACCESS_KEY_ID` and `AWS_SECRET_ACCESS_KEY`. If you don't have these and you want to try out Pernosco on-premises, [contact us](mailto:inquiries@pernos.co).
+
+Make sure the AWS CLI tools are installed and on your `$PATH`. Distribution packages [may fail](https://github.com/aws/aws-cli/issues/2403) so we recommend using
+```
+sudo pip3 install awscli --upgrade
+```
+
+Make sure `docker` is installed and on your `$PATH`. Make sure [rr](https://rr-project.org) is installed and on your path (and that `rr-collect-symbols.py` is on your `$PATH` too).
+
+Clone this respository and make sure the `pernosco` script is on your `$PATH`.
+
+Add your AWS credentials to the environment and run
+```
+pernosco pull
+```
+to pull the Pernosco container images.
+
+Record something with `rr`. Then build a Pernosco database for your recording using
+```
+pernosco build <trace-dir>
+```
+
+Launch the Pernosco client application using
+```
+pernosco serve --storage <storage-dir> --sources <source-dir> <trace-dir>
+```
+where `<source-dir>` is a directory tree containing source files you want to make available in the client. The `--sources` option can be specified multiple times. The `<storage-dir>` should be a directory where Pernosco will store persistent data (e.g. notebook entries) for this trace. If you don't care about keeping state between runs of the client application, you can omit the `--storage` option.
+
+When the client application starts successfully, it will print something like
+```
+Appserver launched at http://172.29.0.2:3000/index.html
+```
+Load that link in your Web browser to debug your recording. When done, press ctrl-C in the terminal to kill the client application.
+
+## Updates
+
+When you want to update Pernosco, pull new revisions from this git repository and then rerun `pernosco pull` (or, by default, `pernosco build` and `pernosco serve` will pull new images as needed).
+
+## Confinement
+
+The Pernosco on-premises product comprises the builder and server containers and the `pernosco` script in this repository. The builder and server are closed-source but are confined to run with the minimum access to the outside world needed to do their work. That confinement is performed using `docker` as configured by the `pernosco` script, which is open-source for your inspection.
+
+The containers require access to the outside world as follows:
+* The builder container requires read/write access to the trace directory subtree (due to `docker` limitations, this directory must be writeable by all users). It has no network access at all (but see below).
+* The server container requires read access to the trace directory subtree and all specified source directory subtrees (due to `docker` limitations, these directories must be readable by all users). It requires read/write access to the storage directory subtree, if specified (you guessed it, due to `docker` limitations, this directory must be writeable by all users). It has a private network that cannot connect to the outside world, so that you can access its Web server from the local machine.
+
+This confinement largely ensures our containers can't modify or leak data even if they were malicious, but technical limitations mean that confinement is not 100% watertight. Known issues:
+* `docker` runs an embedded DNS server accessible to every container which [cannot be disabled](https://github.com/moby/moby/issues/19474). Thus in principle a malicious container could leak information to the outside world via DNS requests. This potential threat could be blocked by blocking DNS on the host.
+* Our Web client uses [CSP](https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP) to confine itself, preventing it from communicating with any Web servers other than the on-premises Pernosco server itself. However, since our Web client is sending the CSP header, a malicious client could stop doing that. This potential threat could be blocked by putting the Web client behind a proxy that forcibly adds the required CSP header.
---
+++ tests/main.py
@@ -0,0 +1,192 @@
+#!/usr/bin/env python3
+
+import glob
+import os
+import shutil
+import signal
+import subprocess
+import sys
+import tempfile
+import time
+
+import selenium
+from selenium import webdriver
+from selenium.common.exceptions import WebDriverException, JavascriptException, NoSuchElementException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+# Installation requirements (all on $PATH):
+# -- latest rr
+# -- git
+# -- Selenium-python
+#
+# Run from the on-prem repo directory.
+
+class CustomException(Exception):
+    pass
+
+TIMEOUT = 60
+
+tmpdir = tempfile.mkdtemp()
+print("Working directory: %s"%tmpdir)
+trace_dir = None
+next_trace_id = 0
+
+clean_env = dict(os.environ, _RR_TRACE_DIR=tmpdir)
+
+def build():
+    subprocess.check_call(['./build.sh'], cwd=testdir)
+
+def record():
+    global trace_dir
+    global next_trace_id
+    subprocess.check_call(['rr', 'record', '%s/out/main'%testdir], env=clean_env)
+    trace_dir = "%s/main-%d"%(tmpdir, next_trace_id)
+    next_trace_id += 1
+
+driver = None
+storage_dir = "%s/storage"%tmpdir
+
+def create_driver():
+    global driver
+    options = webdriver.firefox.webdriver.Options()
+    options.set_preference("network.websocket.delay-failed-reconnects", False)
+    options.set_preference("devtools.console.stdout.content", True)
+    options.log.level = "trace"
+    driver = webdriver.firefox.webdriver.WebDriver(options=options)
+
+def open_browser(url):
+    retries = 0
+    while True:
+        retries += 1
+        if retries > 100:
+            print("Too many retries loading %s, bailing out"%url, file=sys.stderr)
+            raise CustomException("Too many retries")
+
+        try:
+            driver.get(url)
+        except WebDriverException:
+            # The server may not be up yet
+            time.sleep(0.05)
+            continue
+        break
+
+def focus_search():
+    # Use command key to focus search input
+    actions = webdriver.ActionChains(driver)
+    actions.key_down(Keys.CONTROL)
+    actions.send_keys("S")
+    actions.key_up(Keys.CONTROL)
+    actions.perform()
+
+class script_succeeds(object):
+  """An expectation for checking that a script returns true.
+  """
+  def __init__(self, script, expected):
+    self.script = script
+    self.expected = expected
+
+  def __call__(self, driver):
+    try:
+        return driver.execute_script(self.script) == self.expected
+    except JavascriptException:
+        return False
+
+url = None
+server = None
+
+def start_server():
+    global url
+    global server
+    server = subprocess.Popen(['./pernosco', 'serve', '--storage', storage_dir,
+                               '--sources', testdir, '--sources', '/usr', trace_dir],
+                               stdout=subprocess.PIPE, encoding='utf-8')
+    url = None
+    for line in server.stdout:
+        last_word = line.split()[-1]
+        if last_word.startswith("http:"):
+            url = last_word
+            break
+
+test_git_revision = '84861f84a7462c2b4e04b7b41f7f83616c83c8dc'
+subprocess.check_call(['git', 'clone', 'https://github.com/Pernosco/pernosco-submit-test'], cwd=tmpdir)
+testdir = "%s/pernosco-submit-test"%tmpdir
+subprocess.check_call(['git', 'checkout', '-q', test_git_revision], cwd=testdir)
+build()
+record()
+
+subprocess.check_call(['./pernosco', 'pull'])
+
+subprocess.check_call(['./pernosco', 'build', trace_dir])
+
+os.mkdir(storage_dir)
+
+start_server()
+create_driver()
+open_browser(url)
+WebDriverWait(driver, TIMEOUT).until(
+    EC.text_to_be_present_in_element((By.CSS_SELECTOR, ".view.source > .viewTitle"), "_exit.c")
+)
+source_frame = driver.find_element_by_css_selector("#main > .view.source iframe")
+main_window = driver.window_handles[0]
+driver.switch_to.frame(source_frame)
+WebDriverWait(driver, TIMEOUT).until(
+    EC.text_to_be_present_in_element((By.CSS_SELECTOR, "#monaco-container"), "_exit (")
+)
+driver.switch_to.window(main_window)
+
+# Test that notebook text persists
+search_input = driver.find_element_by_css_selector("#searchInput")
+focus_search()
+search_input.send_keys("notebook")
+WebDriverWait(driver, TIMEOUT).until(
+    EC.text_to_be_present_in_element((By.CSS_SELECTOR, "#searchDropdown > *:nth-child(1)"), "otebook")
+)
+search_input.send_keys(Keys.RETURN)
+
+WebDriverWait(driver, TIMEOUT).until(
+    EC.text_to_be_present_in_element((By.CSS_SELECTOR, "#main > .notebook > .contents > div > div:first-child"), "exit")
+)
+first_note = driver.find_element_by_css_selector("#main > .notebook > .contents > div > div.tentative");
+WebDriverWait(driver, TIMEOUT).until(
+    script_succeeds("return document.querySelector('#main > .notebook > .contents > div').classList.contains('focus');", True)
+)
+first_note.click();
+WebDriverWait(driver, TIMEOUT).until(
+    script_succeeds("return document.querySelector('#main > .notebook > .contents > div > div:nth-child(2) > textarea').value;", "")
+)
+
+actions = webdriver.ActionChains(driver)
+actions.send_keys("HelloKitty")
+actions.perform()
+WebDriverWait(driver, TIMEOUT).until(
+    script_succeeds("return document.querySelector('#main > .notebook > .contents > div > div:nth-child(2) > textarea').value;", "HelloKitty")
+)
+# Give the appserver time to write out the data
+time.sleep(2)
+
+driver.get("about:blank")
+
+os.kill(server.pid, signal.SIGINT)
+server.wait()
+
+start_server()
+open_browser(url)
+WebDriverWait(driver, TIMEOUT).until(
+    script_succeeds("return document.querySelector('#main > .notebook > .contents > div > div:nth-child(2) > textarea').value;", "HelloKitty")
+)
+
+driver.quit()
+os.kill(server.pid, signal.SIGINT)
+server.wait()
+
+# Check that docker containers have been cleaned up
+output = subprocess.check_output(['docker', 'ps', '-aq'], encoding='utf-8').strip()
+assert len(output) == 0
+
+print("\nPASS")
+
+# It would be nice to delete 'tmpdir' but we can't actually do that because it contains
+# files and directories owned by another user (the 'pernosco' container user) :-(
